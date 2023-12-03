3686497f0fa4bf0233b4e45b64bae19139fa12ae
Canberk <highpriv@gmail.com>
2023-12-03 12:44:31 +0300

    Javascript'e giriş: console.log & variables & string - number type & 4 işlem

---
+++ 03-12-2023/scripts/script.js
@@ -0,0 +1,58 @@
+//debugger
+console.log("Hello World!");
+
+const firstName = "Ayşe";
+let lastName = "Yıldırım";
+let address;
+let newLastName = 'Yılmaz';
+let age = 26;
+const year = 2023;
+let birthYear;
+
+// MATEMATİKSEL İŞLEMLER
+console.log("Çıkartma sonucu:", 200 - 50);
+console.log("Toplama sonucu:", 450 + 25);
+console.log("Bölme sonucu:", 500 / 10);
+console.log("Çarpma sonucu:", 20 * 10);
+console.log("Bir başka çarpma sonucu:", 20.5 * 10);
+//
+
+
+console.log("Ayşe'nin yaşı:", age);
+console.log("Yıl:", year);
+
+birthYear = year - age;
+console.log(birthYear);
+
+lastName = "Yıldırım" + " " + "Yılmaz"; //string interpolation
+
+
+// const ile oluşturulan değişkenleri değeri daha sonrasında güncellenemeyeceği için
+// başlangıç değeri tanımlamadan bir const değişkeni oluşturulamaz.
+//const country;
+
+console.log("Bir Javascript" + " " + "çıktısı")
+console.log("Soyad: ", lastName);
+
+address = "Istanbul";
+
+console.log(address);
+
+lastName = "Doe";
+
+console.log(lastName)
+
+console.log("script.js dosyası tamamlandı")
+
+// const ile oluşturulan değişkenlerin değeri güncellenemez!
+// firstName = "John";
+
+//var age = 26;
+
+//console.log(firstName);
+
+/*
+console.log("birden fazla")
+console.log("satırı")
+console.log("yorum haline getir")
+*/
