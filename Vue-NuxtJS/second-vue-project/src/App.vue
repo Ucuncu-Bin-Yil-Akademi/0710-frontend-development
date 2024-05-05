b36387326ff4911588ee8929f05c7ecf2db0a293
Canberk <highpriv@gmail.com>
2024-05-05 11:12:28 +0300

    layout

---
+++ Vue-NuxtJS/second-vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <header class="header">
+    <img src="@/assets/logo.svg" height="50" alt="Vue logo" />
+    <h2>UBY Akademi</h2>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.header {
+  width: 100%;
+  background-color: #cccccc;
+  color: rgb(74, 74, 74);
+  padding: 20px;
+  display: flex;
+  align-items: center;
+}
+</style>
